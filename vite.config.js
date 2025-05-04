6311764391350feafe5e7bcabebeec6dba3f84a3
ctrhub <crafterhub.info@gmail.com>
2025-05-04 13:37:44 +0300

    add mock api

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+      // https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
   },
 })
